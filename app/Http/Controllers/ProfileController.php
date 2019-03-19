03358087ee10565526491f29381263355b5af692
Mr-Juni <junipreach2017@gmail.com>
2019-03-20 00:32:58 +0100

    With verification

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Cloudder;
 use App\User;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\ConfirmationLink;
 
 class ProfileController extends Controller
