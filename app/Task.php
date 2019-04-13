be7612cc05a06b29ab4675f6c01360155eb4590b
Mr-Juni <junipreach2017@gmail.com>
2019-04-13 08:15:40 +0100

    small error

--- app/Task.php
+++ app/Task.php
@@ -18,7 +18,7 @@ class Task extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-         'goal_id', 'title', 'description', 'begin_time', 'begin_date', 'due_time', 'due_date', 'completed',
+         'goal_id', 'title', 'description', 'begin_time', 'begin_date', 'due_time', 'due_date', 'reminder', 'completed',
     ];
 
     /**
@@ -27,7 +27,7 @@ class Task extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-       'owner_id','begin_time',
+       'owner_id', 'begin_date',
     ];
 
 
