67b9476cbfcbe384c3483486cd85e9e294ed8dec
Mr-Juni <junipreach2017@gmail.com>
2019-07-25 12:55:59 +0100

    Refactoring

--- resources/views/welcome-new-user.blade.php
+++ resources/views/welcome-new-user.blade.php
@@ -27,7 +27,7 @@
 				   <h4>Confirm Account</h4>
 						<p>Follow this link to activate you account before you can be able to login...</p>
 
-						<a style=" text-decoration: none;padding: 10px;background: #343a40;color: white;font-weight: bold;"href="https://plannerr-fbf4a.firebaseapp.com/Onboard/signup.html?confirm_token={{$user->confirm_token}}" target="_blank">Account Activation</a>
+						<a style=" text-decoration: none;padding: 10px;background: #343a40;color: white;font-weight: bold;"href="https://plannerr-fbf4a.firebasssseapp.com/onboard/signup.html?confirm_token={{$user->confirm_token}}" target="_blank">Account Activation</a>
 				</div>
 
 				<div>
