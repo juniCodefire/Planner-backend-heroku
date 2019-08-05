1737f5dd7f3d9aef81ab90425546d2a5838966d9
Mr-Juni <junipreach2017@gmail.com>
2019-08-05 14:31:10 +0100

    worksapce redefine

--- app/Http/Controllers/UserWorkSpacesController.php
+++ app/Http/Controllers/UserWorkSpacesController.php
@@ -43,7 +43,7 @@ class UserWorkSpacesController extends Controller
         //Check if the user use the name or the username to send a requested
           if (stripos($check_unique_name, " ")) {
             //Return all work sapce with their name and their unique username for the user to choose and send a request
-            $choose_worksapces = WorkSpace::where('title', $request->input('title'))->where('status', 'Public')->get();
+            $choose_workspace = WorkSpace::where('title', $request->input('title'))->where('status', 'Public')->get();
             return response()->json(['data' => ['success' => true, 'message' => 'Choose an ideal workspace from the list',
                                                                    'message-2' => 'If the workspace is not found in the list, it means the workspace is private',
                                                                    'message-3' => 'You can send a message to the worksapce owner to invite you', 'choose_workspace' => $choose_workspace]]);
